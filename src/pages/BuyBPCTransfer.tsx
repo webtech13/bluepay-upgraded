7384803106beba949bf4ff0040ddfeabc4da9ce2
webtech13 <ukmohmmed100@gmail.com>
2025-07-18 07:53:10 +0100

    Update BuyBPCTransfer.tsx

--- src/pages/BuyBPCTransfer.tsx
+++ src/pages/BuyBPCTransfer.tsx
@@ -83,11 +83,11 @@ const BuyBPCTransfer = () => {
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-2xl font-bold">8902612254</p>
+            <p className="text-2xl font-bold">5051090733</p>
             <Button 
               variant="default" 
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-2"
-              onClick={() => handleCopy("8902612254", "Account Number")}
+              onClick={() => handleCopy("5051090733", "Account Number")}
             >
               <Copy size={18} />
               Copy
@@ -97,12 +97,12 @@ const BuyBPCTransfer = () => {
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Bank Name</p>
-          <p className="text-2xl font-bold">PALMPAY</p>
+          <p className="text-2xl font-bold">MONIEPOINT MFB</p>
         </div>
 
         <div className="mb-4 border-t pt-4">
           <p className="text-gray-500 text-sm">Account Name</p>
-          <p className="text-2xl font-bold">EBUKA SABASTINE</p>
+          <p className="text-2xl font-bold">MAHMUD MUHAMMAD</p>
         </div>
       </div>
 
